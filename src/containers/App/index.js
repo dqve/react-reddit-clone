fdeb6cf2dfd5413dd5345dd908351000fe81a378
Ayoola David <ayooladavid@gmail.com>
2020-05-03 20:26:40 +0100

    Add firebase connection

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -3,7 +3,26 @@ import * as firebase from "firebase"
 import config from './firebase-config'
 
 
-class App extends Component{
+class App extends Component {
+
+    constructor() {
+        super();
+        // Initialize Firebase
+        firebase.initializeApp(config);
+    }
+
+    componentWillMount() {
+        let postsRef = firebase.database().ref('posts');
+        let _this = this;
+        postsRef.on('value', function(snapshot) {
+            console.log(snapshot.val());
+            _this.setState({
+                posts: snapshot.val(),
+                loading: false
+            });
+        });
+    }
+
 
 	render(){
 		return(
