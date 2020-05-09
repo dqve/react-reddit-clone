e35d352e3f24336fab40a2350949dc3a5c21b930
Ayoola David <ayooladavid@gmail.com>
2020-05-09 01:47:59 +0100

    edits

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+// eslint-disable-next-line 
 import App from './containers/App';
 import Routes from './components/routes';
 import { BrowserRouter } from 'react-router-dom';
